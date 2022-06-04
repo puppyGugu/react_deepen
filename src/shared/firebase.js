7efc054d53a31725f9750cc3761b33cf176cd993
puppyGugu <lbs7230@naver.com>
2022-06-04 15:10:44 +0900

    로그인을 위한 firebase.js 파일 추가

---
+++ src/shared/firebase.js
@@ -0,0 +1,18 @@
+import { initializeApp } from 'firebase/app';
+import { getAuth } from "firebase/auth";
+
+// TODO: Replace the following with your app's Firebase project configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyCHPPKTYa2PKrspp4Oyx-d8uwEf4FqYqwc",
+    authDomain: "authex-bba7f.firebaseapp.com",
+    projectId: "authex-bba7f",
+    storageBucket: "authex-bba7f.appspot.com",
+    messagingSenderId: "275447860865",
+    appId: "1:275447860865:web:319683eb9d93c949734780",
+    measurementId: "G-LTPB0HKB9H"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth();
+export default app;
